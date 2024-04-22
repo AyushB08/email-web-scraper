4309c7bfee28d29f237dc5a47e774355f3b1c068
Ayush B <102773944+AyushB08@users.noreply.github.com>
2024-04-21 20:20:38 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import requests
+
+URL = "https://engineering.ucsc.edu/departments/computer-science-and-engineering/people/" # webpage to scrape
+page = requests.get(URL)
+
+text = page.text.lower()
+
+print(text)
+
+index = 0
+emails = []
+
+domain_name = "@ucsc.edu" # domain name
+unique_string = "@ucsc.edu<\""  # unique string that tells the algorithm there is an email
+ending_string = ">"  # ending string that tells the algorithm to stop
+
+while index < len(text):
+
+    next_email = text.find(unique_string, index)
+    if next_email == -1:
+        break
+    email = ""
+    counter = next_email - 1
+    while counter >= 0:
+        if text[counter] == ending_string:
+            break
+        email = text[counter] + email
+        counter -= 1
+
+    email += domain_name
+
+    emails.append(email)
+    index = next_email + 1
+
+for i in range(len(emails)):
+    print(emails[i])
+
+# simply copy and paste the emails into the recipients section of your email
